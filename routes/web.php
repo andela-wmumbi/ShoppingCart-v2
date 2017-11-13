0bd78889409c16596ce338f049a2826ee4de3038
andela-wmumbi <winnie.mumbi@andela.com>
2017-11-13 21:47:48 +0300

    Setting up models

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| routerlication Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you can register all of the routes for an routerlication.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
@@ -13,4 +13,15 @@
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+
+/**
+ *  Routes for items
+ */
+$router->group(['prefix' => 'api'], function ($router) {
+    $router->get('items', 'ItemsController@add');
+    $router->post('items', 'ItemsController@all');
+    $router->put('items/{id}', 'ItemsController@put');
+    $router->delete('items/{id}', 'ItemsController@remove');
 });
